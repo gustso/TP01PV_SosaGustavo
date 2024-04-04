70b39eeb724895b44d51e8dd03ec1f1418e97a9b
gustso <gustso@gustsoNT>
2024-04-04 15:16:39 -0300

    Ejercicio 01 Resuelto - Gustavo Sosa

---
+++ src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		int numero01 = 5;
+		int numero02 = 6;
+		double promedio = (double)(numero01 + numero02)/2;
+		
+		System.out.println(promedio);
+		*/
+		float primerNumero = 8;
+		float segundoNumero = 5;
+		float tercerNumero = 1;
+		float cuartoNumero = 20;
+		float quintoNumero = 9;		
+		
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println(primerNumero+", "+segundoNumero);
+		System.out.println("El promedio es: "+ promedio );
+		
+	}
+
+}
